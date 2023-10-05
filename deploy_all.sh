b8221bfc066dd5d8303a9d2b635cb376e1ed9134
Reza Soltani <soltanireza65@gmail.com>
2023-10-05 18:26:27 +0330

    remove apps

--- deploy_all.sh
+++ deploy_all.sh
@@ -4,12 +4,6 @@ DEPLOY_MODE=$1
 
 git pull
 
-# add app to list
-[ ! -d "apps/profile" ] && git clone git@github.com:pondersource/solidProfileEditor.git apps/profile
-
-# add app to list
-git --git-dir=apps/profile/.git --work-tree=./apps/profile pull origin main
-
 docker volume create caddy_data
 docker volume create portainer_data
 
